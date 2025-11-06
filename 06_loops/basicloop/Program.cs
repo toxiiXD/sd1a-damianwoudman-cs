5186530ceb602211b944d082da2604c0d0ed84ad
Your Name <toxianxd@gmail.com>
2025-11-06 09:22:22 +0100

    inleveren2

---
+++ 06_loops/basicloop/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06_loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int[] nummers(
+            
+
+             );
+        
+       for (int i = 0; i < nummers.length; i++)
+        {
+
+            Console.WriteLine(nummers);
+
+       }
+    
+
+    }
+}
