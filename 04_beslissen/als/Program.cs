0c2bcd439e51f8765df06e8375f15281fc436e5f
Your Name <toxianxd@gmail.com>
2025-11-06 09:47:03 +0100

    inleveren-3

---
+++ 04_beslissen/als/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace als;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("- ur searching for the key.");
+        Console.WriteLine("- did u find the key? (Y/N)");
+        bool sleutelkwijt = Console.ReadLine() == "N";
+        
+
+        if (sleutelkwijt == true)
+        {
+        
+            Console.WriteLine("u didnt find the key.");
+            Console.WriteLine("door stays closed.");
+        }
+        else
+        {
+            Console.WriteLine("u have found the key");
+            Console.WriteLine("door is open");
+        }
+    //console hier
+
+    }
+}
---
+++ 04_beslissen/en/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace en;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("- is the door closed? Y/N");
+        bool doorNeedsKey = Console.ReadLine() == "Y";
+        if (doorNeedsKey == true)
+        {
+            Console.WriteLine("- do u have the key? Y/N");
+            bool hasKey = Console.ReadLine() == "Y";
+            if (hasKey == true)
+            {
+                Console.WriteLine("- u use the key to open the door.");
+            }
+            else
+            {
+                Console.WriteLine("- u dont have a key.");
+                Console.WriteLine("- the door stays closed.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("- u open the door.");
+        }
+    }
+}
