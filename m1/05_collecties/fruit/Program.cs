69b773ac36347ebc94757a10f32f58b921de65c3
Your Name <toxianxd@gmail.com>
2025-11-21 11:56:07 +0100

    inleveren345

---
+++ m1/05_collecties/fruit/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace fruit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string[] fruit = new string[] { "appel", "banaan" };
+        
+    }
+}
