f0db66d4cef2c180124314e4ac02b6889c0febb5
Your Name <toxianxd@gmail.com>
2025-11-24 09:13:39 +0100

    e

--- m2/01_functions/Program.cs
+++ m2/01_functions/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace _01_functions;
 
-class Program
+internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Program program = new Program();
+        program.run();
+        
+    }
+    internal void run()
+    {
+        console.writeline("Dit is nu de start van mijn programma");
+        return;
     }
 }
