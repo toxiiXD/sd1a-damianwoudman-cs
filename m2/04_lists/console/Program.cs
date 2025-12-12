db2063b1c449378b73b5e172a878632b3b2b69a6
Your Name <toxianxd@gmail.com>
2025-12-12 13:07:45 +0100

    afwkgfhwkfja

--- m2/04_lists/console/Program.cs
+++ m2/04_lists/console/Program.cs
@@ -2,14 +2,17 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
+    
         string[] characters =
             { "Pac-Man", "Samus", "Crash Bandicoot", "Spyro the Dragon", "Donkey Kong", "Mario",
-            "Luigi", "Astro", "Solid Snake", "Master Chief", "Kratos", "Sonic the Hedgehog", "Link" };
+            "Luigi", "Astro", "Solid Snake", "Master Chief", "Kratos", "Sonic the Hedgehog", "Link" }
 
 
+        List<string> characterlist = new List<string>();
+        
+        characterlist.Add("Link");
+        for (int i = 0; i < characters.Length; i++) {characterlist.Add(characters) };
 
-
-    }
+        Console.WriteLine(characterlist);
+    
 }
