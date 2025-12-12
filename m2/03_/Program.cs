848358b95aef3261c99172d56d1cfa0f9400940c
Your Name <toxianxd@gmail.com>
2025-12-12 11:58:15 +0100

    hef

---
+++ m2/03_/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _03_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    
+}
---
+++ m2/03_/QuizVraag.cs
@@ -0,0 +1,11 @@
+class QuizVraag
+{
+    internal string vraag;
+    internal string Antwoord;
+
+    internal QuizVraag(string vraag, string Antwoord)
+    {
+        this.vraag = vraag;
+        this.Antwoord = Antwoord;
+    }
+}
